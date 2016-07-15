a8779a36d50bdb663d858b8e4fda0c0804f1ca08
Makotro <leri6@hotmail.com>
2016-07-15 20:21:13 +0300

    Renamed the test class that only pukes out the monthly wages

--- src/test/java/com/marantle/nutcracker/RunSimpleTest.java
+++ src/test/java/com/marantle/nutcracker/RunSimpleTest.java
@@ -1,27 +1,22 @@
 package com.marantle.nutcracker;
 
-import com.marantle.nutcracker.model.Salary;
 import com.marantle.nutcracker.parser.DataParser;
 import com.marantle.nutcracker.repository.NutRepo;
-import com.marantle.nutcracker.util.MyUtilities;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.net.URISyntaxException;
-import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * Created by Marko on 11.7.2016.
  */
-public class TestMain {
+public class RunSimpleTest {
 
     static NutRepo repo = NutRepo.getInstance();
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         DataParser dataParser = new DataParser();
 
-        dataParser.parseData("HourList201403.csv");
+        dataParser.parseData("hourlist.csv");
         repo.setPersons(dataParser.getPersons());
         repo.setWorkShifts(dataParser.getShifts());
         repo.setWorkDays(dataParser.getWorkdays());
